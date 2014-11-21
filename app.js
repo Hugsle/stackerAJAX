e259fd6021c88af1c9c5c7dceee369e5f50f3b12
Jack C. <popjack4321@gmail.com>
2014-11-20 17:43:47 -0800

    1
    1

--- app.js
+++ app.js
@@ -86,6 +86,13 @@ var getUnanswered = function(tags) {
 		var errorElem = showError(error);
 		$('.search-results').append(errorElem);
 	});
+
+
+var getTopAnswerers = function() {
+	
+}
+
+
 };
 
 
